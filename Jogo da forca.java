16af6ab61ff56ab1426def49c7c59ea0dc0ae37a
Peixesvetoriais <140614561+Peixesvetoriais@users.noreply.github.com>
2024-10-02 09:18:14 -0300

    Update[2]Jogo da forca.java
    Novo sistema de palavras:
    - Pode inserir letras ou palavras completas.
    - Possui um conversor de String para Char dependendo do tamanho da String.

--- Jogo da forca.java
+++ Jogo da forca.java
@@ -8,13 +8,15 @@ public class Jogodaforca {
     public static void main(String[] args) {
         // Iniciando o Scanner
         Scanner scanchar = new Scanner(System.in);
+        Scanner scanstring = new Scanner(System.in);
         // Iniciando as variaveis de vitoria
         boolean vitoria = false;
         boolean derrota = false;
         // Iniciando as variaveis
         int erros = 0;
         int loop = 0;
-        char letra;
+        char letra = 'a';
+        String _letra = "a";
         String palavracerta;
         String palavra = "0";
         String resto = "";
@@ -60,8 +62,19 @@ public class Jogodaforca {
         // Criptografa a palavra selecionada
         palavracerta = "_".repeat(palavra.length());
         do {
+            // Verifica se o jogo já terminou
+            if (palavracerta.equals(palavra)) {
+                loop++;
+                vitoria = true;
+                continue;
+            }
+            // Verifica se você errou 5 vezes e termina o jogo
+            if (erros >= 5) {
+                loop++;
+                derrota = true;
+                continue;
+            }
             // Começa o desenho da forca
-            System.out.println(palavra);
             System.out.println("|-----|");
             // Checa a variavel erros e constroi o boneco
             switch (erros) {
@@ -86,34 +99,37 @@ public class Jogodaforca {
             }
             System.out.println("|     ");
             System.out.println("|  " + palavracerta);
-            // Verifica se o jogo já terminou
-            if (palavracerta.equals(palavra)) {
-                loop++;
-                vitoria = true;
-                continue;
-            }
-            // Verifica se você errou 5 vezes e termina o jogo
-            if (erros >= 5) {
-                loop++;
-                derrota = true;
-                continue;
-            }
             // Entrada de dados
-            System.out.print("\n" + "Escreva uma letra: ");
+            System.out.print("\n" + "Escreva uma letra ou uma palavra: ");
             // Iniciando variaveis e o scan para a forca
-            letra = scanchar.next().charAt(0);
+            _letra = scanstring.nextLine();
             resto = "";// Variável responsável por alterar a criptografia da palavra selecionada
             boolean erro = true;
-            // Verificador da palavra selecionada
-            for (int i = 0; i < palavra.length(); i++) { // Vai passar o loop por todas as letras
-                if (palavra.charAt(i) == letra) {// Verifica se a letra digitada tem na palavra selecionada
-                    resto += String.valueOf(letra); // Soma a letra a forca
-                    erro = false;
-                    continue;
-                }
-                resto += String.valueOf(palavracerta.charAt(i)); // Soma o resto da criptografia
+            // Verifica se é uma letra
+            if (_letra.length()<=1) {
+                letra = _letra.charAt(0);
+                for (int i = 0; i < palavra.length(); i++) { // Vai passar o loop por todas as letras
+                    if (palavra.charAt(i) == letra) {// Verifica se a letra digitada tem na palavra selecionada
+                        resto += String.valueOf(letra); // Soma a letra a forca
+                        erro = false;
+                        continue;
+                    }
+                    resto += String.valueOf(palavracerta.charAt(i)); // Soma o resto da criptografia
+                } 
+            // Verifica se é uma palavra
+            } else if (_letra.length()>1 && _letra.length()==palavra.length()){
+                for (int i = 0; i < 1; i++) { // Vai passar o loop pela palavra
+                    if (palavra.equals(_letra)) {// Verifica se a palavra digitada é correta
+                        resto += (_letra); // Soma a palavra a forca
+                        erro = false;
+                        continue;
+                    }
+                    resto += (palavracerta); // Soma o resto da criptografia
+                } 
+            } else {
+                System.out.print("Você digitou uma palavra incorreta, tente novamente!");
+                continue;
             }
-            // Verifica o erro e soma na variável
             if (erro) {
                 erros++;
             }
